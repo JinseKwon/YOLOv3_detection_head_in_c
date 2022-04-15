d2f89b5868b715b9d201a02f009d131a998b7886
JinseKwon <--global>
2022-04-15 18:56:18 +0900

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ yolov3.c yolov3_det_head.c -o yolov3 -I. -I../onnxruntime-linux-x64-1.11.0/include/ -fPIC ../onnxruntime-linux-x64-1.11.0/lib/libonnxruntime.so.1.11.0 -lm `pkg-config --libs opencv`
+g++ yolov3.c yolov3_det_head.c -o yolov3 -I. -I../onnxruntime-linux-x64-1.11.0/include/ -fPIC ../onnxruntime-linux-x64-1.11.0/lib/libonnxruntime.so -lm `pkg-config --libs opencv`
 
 if [ $# -eq 0 ] ; then
 	./yolov3 yolov3.onnx dog.jpg cpu
